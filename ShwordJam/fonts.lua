d03bf949d3dfd7709378d5d5e725c7610c9932d4
Joel Schumacher <joelschum@gmail.com>
2018-02-12 03:53:06 +0100

    Add missing fonts.lua

---
+++ ShwordJam/fonts.lua
@@ -0,0 +1,9 @@
+local fonts = {}
+
+fonts.default = lg.newFont(18)
+fonts.big = lg.newFont(22)
+fonts.bigger = lg.newFont(26)
+fonts.huge = lg.newFont(32)
+fonts.huger = lg.newFont(36)
+
+return fonts
