a5ca4a393b03cae1a711b249ea396fbe2ffca322
Joel Schumacher <joelschum@gmail.com>
2018-02-12 18:02:27 +0100

    Add gameobject ids and :removeFromWorld

--- ShwordJam/gameobject/init.lua
+++ ShwordJam/gameobject/init.lua
@@ -6,7 +6,12 @@ local GameObject = class("GameObject")
 
 function GameObject.resetWorld()
     GameObject.world = {}
+    GameObject.idMap = {}
     GameObject.collider = HC.new(1)
+end
+
+function GameObject.getById(id)
+    return GameObject.idMap[id]
 end
 
 local function gameObjectCmp(a, b)
@@ -36,7 +41,20 @@ end
 
 function GameObject:initialize()
     table.insert(GameObject.world, self)
+    self.id = math.floor(lm.random(0, 2^52))
+    GameObject.idMap[self.id] = self
     self.depth = 0
+end
+
+function GameObject:removeFromWorld()
+    local index = nil
+    for i, obj in ipairs(GameObject.world) do
+        if obj == self then
+            index = i
+        end
+    end
+    table.remove(GameObject.world, index)
+    GameObject.idMap[self.id] = nil
 end
 
 function GameObject:update()
@@ -44,5 +62,6 @@ end
 
 function GameObject:draw()
 end
+
 
 return GameObject
